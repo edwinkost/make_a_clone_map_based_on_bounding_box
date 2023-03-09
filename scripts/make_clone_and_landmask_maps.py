101c66dafa69b63d2e48b331e015a0d91f1993f5
Edwin Sutanudjaja <sutan101@UU078667-ubuntu>
2023-03-09 16:13:49 +0100

    include clump

--- scripts/make_clone_and_landmask_maps.py
+++ scripts/make_clone_and_landmask_maps.py
@@ -147,7 +147,7 @@ def main():
     print("using only the largest clump") 
     clump_ids   = pcr.clump(bounding_box_catchment)
     clump_areas = pcr.areatotal(pcr.scalar(bounding_box_catchment), clump_ids)
-    bounding_box_catchment = pcr.ifthen(clump_areas == pcr.areamaximum(clump_areas), pcr.boolean(1.0))
+    bounding_box_catchment = pcr.ifthen(clump_areas == pcr.mapmaximum(clump_areas), pcr.boolean(1.0))
     bounding_box_catchment = pcr.ifthen(bounding_box_catchment, bounding_box_catchment)
     pcr.report(bounding_box_catchment, out_mask_file)
     
